9801eb5504feeb25f3358bb24fd383d2a0e8446e
nayanzinaalexandra <alexandranayanzina@gmail.com>
2022-02-23 00:06:56 +0200

    Test 'test_guest_can_go_to_login_page' was added. Extra: transition between pages

--- test_main_page.py
+++ test_main_page.py
@@ -16,8 +16,10 @@ login_link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 #     page.open()
 #     page.should_be_login_link()
 
-def test_guest_should_see_login_page(browser):
-    page = LoginPage(browser, login_link)
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_page()
-    time.sleep(5)
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    time.sleep(3)
