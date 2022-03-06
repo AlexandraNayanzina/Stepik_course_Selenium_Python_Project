a6d2fb3df9d5b8c71d368f5f01709c6a9425aaa6
nayanzinaalexandra <alexandranayanzina@gmail.com>
2022-03-06 14:55:56 +0200

    parameterized test "guest can add product to basket" was added

--- pages/locators.py
+++ pages/locators.py
@@ -7,4 +7,12 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
+class ProductPageLocators():
+    ADD_TO_BASKET_LINK = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alert.alert-safe.alert-noicon.alert-info  > .alertinner > p strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main > p.price_color")
+    PRODUCT_IN_BASKET = (By.CSS_SELECTOR, ".alert.alert-safe.alert-noicon > .alertinner  >  strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main > h1")
+
+
 
